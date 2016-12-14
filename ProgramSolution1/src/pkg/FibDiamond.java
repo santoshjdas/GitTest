6f16e333a015a10c0852234b22bb986e9f7e84eb
santoshjdas <dsantosh.das@gmail.com>
2016-12-14 22:28:21 +0530

    commit

--- ProgramSolution1/src/pkg/FibDiamond.java
+++ ProgramSolution1/src/pkg/FibDiamond.java
@@ -1,147 +1,88 @@
 package pkg;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class FibDiamond {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		ArrayList<Integer> fib =new ArrayList<Integer>();
-
-		int n1=0,n2=1,n3=0,k,count=11;    
-
-		String fullString="1";
-
-		//System.out.print(n1+" "+n2);
-
-		for(k=0;k<count;++k) 
-		{    
-		  n3=n1+n2;  
-		//  System.out.print(" "+n3);
-		  fib.add(n3);
-		  fullString=fullString+n3+"+";
-		  n1=n2;
-		  n2=n3;    
+		ArrayList<Integer> fib = new ArrayList<Integer>();
+		ArrayList<String> outputLines = new ArrayList<String>();
+		int n1 = 0, n2 = 1, n3 = 0, k, count = 11;
+		String fullString = "1";
+		for (k = 0; k < count; ++k) {
+			n3 = n1 + n2;
+			fib.add(n3);
+			fullString = fullString + n3 + "+";
+			n1 = n2;
+			n2 = n3;
+		}
+		System.out.print("\n");
+		for (int i = 1; i < 9; i += 2) {
+			String tempLine = "";
+			for (int j = 0; j < 8 - i / 2; j++)
+				tempLine = tempLine + " ";
+			if (fullString.startsWith("+")) {
+				fullString = fullString.substring(1);
+			}
+			for (int j = 0; j < i; j++) {
+				tempLine = tempLine + (fullString.substring(0, 1));
+				fullString = fullString.substring(1);
+			}
+			for (int l = tempLine.length(); l < 12; l++) {
+				tempLine = tempLine + " ";
+			}
+			outputLines.add(tempLine);
 		}
 
-		 // System.out.println(fullString);
-
-
+		for (int a = 5; a > 0; a -= 2) {
+			String tempLine = "";
+			for (int b = 0; b < 8 - a / 2; b++)
+				tempLine = tempLine + " ";
+			if (fullString.startsWith("+")) {
+				fullString = fullString.substring(1);
+			}
+			for (int b = 0; b < a; b++) {
+				tempLine = tempLine + fullString.substring(0, 1);
+				fullString = fullString.substring(1);
+			}
+			for (int l = tempLine.length(); l < 12; l++) {
+				tempLine = tempLine + " ";
+			}
+			outputLines.add(tempLine);
+		}
 		System.out.print("\n");
-
-		      for (int i = 1; i < 9; i += 2) {
-		      for (int j = 0; j < 8 - i / 2; j++)
-	        
-		    	  System.out.print(" ");
-
-		      if(fullString.startsWith("+")){
-
-		      fullString=fullString.substring(1);
-
-		      }
-
-		      for (int j = 0; j < i; j++){
-
-
-		      //System.out.println(fullString);
-
-		        System.out.print(fullString.substring(0, 1));
-
-		        fullString=fullString.substring(1);
-
-		      }
-
-		      System.out.print("\n");
-
-		    }
-
-
-		    for (int a =5 ; a > 0; a -= 2) {
-
-		      for (int b = 0; b < 8 - a / 2; b++)
-
-		        System.out.print(" ");
-
-		      if(fullString.startsWith("+")){
-
-		      fullString=fullString.substring(1);
-
-		      }
-
-		      for (int b = 0; b < a; b++){
-
-		        System.out.print(fullString.substring(0, 1));
-
-		        fullString=fullString.substring(1);
-
-		      }
-
-		      System.out.print("\n");
-
-		    }
-
-		
-		
-		
-		
-		System.out.print("\n");
-
-		String fullString1="";
-
+		String fullString1 = "";
+		Iterator<String> iterator = outputLines.iterator();
 		for (int a = 1; a < 9; a += 2) {
-
-		 
-
-		      for (int b = 0; b < 8 - a / 2; b++)
-
-		        System.out.print(" ");
-
-		     
-
-		      int number=0;
-
-		      for (int b = 0; b < a; b++){
-
-		      fullString1="UNOCOIN";
-
-
-		      number++;
-
-		      //System.out.println(fullString);
-
-		      }
-
-		        System.out.print(fullString1.substring(0,number));
-
-
-		      System.out.print("\n");
-
-		    }
-
-
-		    for (int a =5 ; a > 0; a -= 2) {
-
-		      for (int b = 0; b < 8 - a / 2; b++)
-
-		        System.out.print(" ");
-
-		      int number=0;
-
-		      for (int b = 0; b < a; b++){
-
-		      fullString1="UNOCOIN";
-
-		      number++;
-
-		      }
-
-		        System.out.print(fullString1.substring(0,number));
-
-
-		      System.out.print("\n");
-
-		    }
-
+			if (iterator.hasNext()) {
+				System.out.print(iterator.next());
+			}
+			for (int b = 0; b < 8 - a / 2; b++)
+				System.out.print(" ");
+			int number = 0;
+			for (int b = 0; b < a; b++) {
+				fullString1 = "UNOCOIN";
+				number++;
+				// System.out.println(fullString);
+			}
+			System.out.print(fullString1.substring(0, number));
+			System.out.print("\n");
+		}
+		for (int a = 5; a > 0; a -= 2) {
+			if (iterator.hasNext()) {
+				System.out.print(iterator.next());
+			}
+			for (int b = 0; b < 8 - a / 2; b++)
+				System.out.print(" ");
+			int number = 0;
+			for (int b = 0; b < a; b++) {
+				fullString1 = "UNOCOIN";
+				number++;
+			}
+			System.out.print(fullString1.substring(0, number));
+			System.out.print("\n");
+		}
 	}
 	}
 
